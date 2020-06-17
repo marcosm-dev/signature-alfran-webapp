7635fa756133c25ce449c4073483fe26a809e71a
Marcos Marrero <marcos@Marcos.local>
2020-06-17 02:05:25 +0100

    404 Pagina no encontrada

---
+++ src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text-center">
+    <h1>404 PAGINA NO ENCONTRADA</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
